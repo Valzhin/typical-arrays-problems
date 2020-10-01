9d6d8a0e3429c5044b16f54a8edf3537c3114d09
Valzhin <70908623+Valzhin@users.noreply.github.com>
2020-10-01 23:02:17 +0300

    fix:  function changed

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-    if (array != undefined || array[0] != undefined) {return array.reduce((a, b) => a + b, 0) / array.length}
-    else {return 0};
+    if (array === undefined || array[0] === undefined) return 0; 
+    return array.reduce((a, b) => a + b, 0) / array.length;
 }
